35c43cd6e533785ee2dd8e751dd72c7145f6854f
waynetipsy <jacketoniru@gmail.com>
2024-02-07 12:35:34 +0100

    Nav Code

---
+++ app/src/main/java/app/ify/navigationsample/SecondScreen.kt
@@ -0,0 +1,48 @@
+package app.ify.navigationsample
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SecondScreen(navigateToFirstScreen:()->Unit ) {
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text("This is the Second screen", fontSize = 24.sp)
+        Text("Welcome", fontSize = 24.sp)
+        Button(onClick = {
+      navigateToFirstScreen()
+        }) {
+            Text("Go to First screen")
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun ScreenPreview() {
+    SecondScreen({})
+}
+
+
